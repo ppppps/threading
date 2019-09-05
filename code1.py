5c137329fcd038fea2c8057ebb1fdbb20d66175d
ppppps <2019+ppppps@users.noreply.github.com>
2019-09-05 21:31:26 +0800

    多进程,通过队列返回

--- code1.py
+++ code1.py
@@ -2,14 +2,24 @@ import multiprocessing as mp
 import threading as td
 
 
-def job(a, b):
-    print('aaaa')
+def job(q):
+    res = 0
+    for i in range(1000):
+        res += i + pow(i, 2) + pow(i, 3)
+    q.put(res)
 
 
 # t1 = td.Thread(target=job, args=(1,2))
 if __name__ == '__main__':
-    p1 = mp.Process(target=job, args=(1, 2))
-    # t1.start()
+    q = mp.Queue()
+
+    p1 = mp.Process(target=job, args=(q,))
+    p2 = mp.Process(target=job, args=(q,))  # 这个逗号不能少！！
+
     p1.start()
-    # t1.join()
+    p2.start()
     p1.join()
+    p2.join()
+    res1 = q.get()
+    res2 = q.get()
+    print(res1 + res2)
